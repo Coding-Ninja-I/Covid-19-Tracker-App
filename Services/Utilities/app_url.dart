79a22045c05e7431a2a915c0b840df196df6fea0
Debangi Bhaumik <53263649+Coding-Ninja-I@users.noreply.github.com>
2022-09-08 17:15:39 +0530

    Create app_url.dart

---
+++ Services/Utilities/app_url.dart
@@ -0,0 +1,11 @@
+
+class AppUrl{
+
+  //This is our base url
+  static const String  baseUrl = 'https://disease.sh/v3/covid-19/';
+
+  //fetch world covid states
+  static const worldStatesApi = baseUrl + 'all';
+  static const countriesList = baseUrl + 'countries';
+
+}
